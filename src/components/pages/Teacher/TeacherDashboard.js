5d0025d0bd7d8d2f75a19c7136594ab30029d029
Joon <jong@jongs-mbp.lan>
2021-01-19 14:15:22 -0800

    Finish setting up TeacherDashboard

--- src/components/pages/Teacher/TeacherDashboard.js
+++ src/components/pages/Teacher/TeacherDashboard.js
@@ -1,7 +1,146 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import { connect } from 'react-redux';
+import {
+  Link,
+  NavLink,
+  // Redirect,
+  // BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Village from '../Village/Village.component.js';
+import VillageForm from '../Village/VillageForm.js';
+import Schools from '../School/Schools.component.js';
+import SchoolForm from '../School/SchoolForm.js';
+import TeacherProfile from './TeacherProfile/Profile.js';
+import ProfileForm from './TeacherProfile/ProfileForm.js';
+// import TeacherNav from './Drawer';
+// import TestComponent from './TestComponent';
+import { Drawer, Button } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import './TeacherDashboard.css';
+import {
+  menuButton,
+  menuIcon,
+  menuMove,
+  Dashboard,
+} from './TeacherDashboard.style';
+import Logout from '../../Logout.js';
+// import MentorPairings from './Mentees/Mentees.js';
+import Mentees from '../Headmaster/Mentees/Mentees';
 
-const TeacherDashboard = () => {
-  return <div></div>;
-};
+function TeacherDashboard() {
+  const [visible, setVisible] = useState(true);
+  const [desktop, setDesktop] = useState(true);
 
+  useEffect(() => {
+    if (window.innerWidth <= 800 || document.documentElement.width <= 800) {
+      setDesktop(false);
+      setVisible(false);
+    } else {
+      setDesktop(true);
+    }
+  }, []);
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  // Todo: this needs to be converted to a mediaquery and removed from here
+  window.addEventListener('resize', () => {
+    if (window.innerWidth <= 800 || document.documentElement.width <= 800) {
+      setDesktop(false);
+      setVisible(false);
+    } else {
+      setDesktop(true);
+      setVisible(true);
+    }
+  });
+
+  return (
+    <div>
+      <Dashboard>
+        <Switch>
+          <Route path="/mentor-pairings" component={Mentees} />
+          <Route exact path="/profile" component={TeacherProfile} />
+          <Route path="/profile/edit/:id" component={ProfileForm} />
+          <Route path="/mentor-advisor" />
+          <Route path="/school-village">
+            <Village />
+            <Schools />
+          </Route>
+          <Route
+            exact
+            path="/village/edit/:villageId"
+            component={VillageForm}
+          />
+          <Route exact path="/school/edit/:schoolId" component={SchoolForm} />
+          <Route path="/library" />
+          <Route path="/logout" component={Logout} />
+        </Switch>
+      </Dashboard>
+
+      {desktop ? null : (
+        // inline style to force animation
+        <div style={visible ? menuMove : menuIcon}>
+          <Button
+            type="primary"
+            style={menuButton} // inline style to override Ant Design
+            onClick={() => setVisible(!visible)}
+            icon={<MenuOutlined />}
+          >
+            Menu
+          </Button>
+        </div>
+      )}
+      <div>
+        <Drawer
+          placement={desktop ? 'left' : 'bottom'}
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+          mask={false}
+          width={desktop ? 300 : 500}
+          height={500}
+        >
+          <h2>Hello, Teacher!</h2>
+
+          <NavLink to="/dashboard" onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">Home</button>
+          </NavLink>
+          <NavLink to="/profile" onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">Profile</button>
+          </NavLink>
+          <NavLink to={'/mentor-pairings'} onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">Mentor Pairings</button>
+          </NavLink>
+          <NavLink to="/mentor-advisor" onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">Mentor Advisor</button>
+          </NavLink>
+          <NavLink to="/school-village" onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">School/Village</button>
+          </NavLink>
+          <NavLink to="/library" onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">Library</button>
+          </NavLink>
+          <Link to="/logout" onClick={() => setVisible(true)}>
+            <button className="btn l2-btn menuLinks">Logout</button>
+          </Link>
+        </Drawer>
+
+        {/* <TeacherNav /> */}
+      </div>
+    </div>
+  );
+}
+
+// const mapStateToProps = state => {
+//   return {
+//     loggedIn: state.authReducer.loggedIn,
+//     // userId: state.authReducer.userId,
+//     // role: state.authReducer.role,
+//   };
+// };
+
+// export default connect(mapStateToProps, {})(TeacherDashboard);
 export default TeacherDashboard;
