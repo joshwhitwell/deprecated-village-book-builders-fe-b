f94342d3a734b6aff1fc152846850701b96f598c
devbeau <48367657+devbeau@users.noreply.github.com>
2021-01-28 22:11:31 -0500

    removed debug logs

--- src/state/reducers/headmasterReducer.js
+++ src/state/reducers/headmasterReducer.js
@@ -10,7 +10,7 @@ import {
   FETCH_MENTEE_START,
 } from '../actions/actionTypes';
 
-import { debugLog } from '../../utils/debugMode.js'; //
+// import { debugLog } from '../../utils/debugMode.js';
 
 const initialState = {
   villageData: {},
@@ -24,31 +24,24 @@ const reducer = (state = initialState, action) => {
   // console.log('HEADMASTERREDUCER.js, action type & payload:', action.type, action.payload);
   switch (action.type) {
     case FETCH_HEADMASTER_SCHOOLS:
-      debugLog(action.type, action.payload);
       return { ...state, schoolData: action.payload };
     case FETCH_HEADMASTER_PROFILE:
-      debugLog(action.type, action.payload);
       return { ...state, headmasterProfile: action.payload };
     case FETCH_VILLAGE:
-      debugLog(action.type, action.payload);
       return {
         ...state,
         villageData: action.payload,
       };
     case FETCH_PENDING_TEACHERS:
-      debugLog(action.type, action.payload);
       return { ...state, pendingTeachers: action.payload };
     case FETCH_MENTEE_SUCCESS:
-      debugLog(action.type, action.payload);
       return {
         ...state,
         mentees: action.payload,
       };
     case FETCH_MENTEE_START:
-      debugLog(action.type, action.payload);
       return { ...state };
     case FETCH_MENTEE_FAILURE:
-      debugLog(action.type, action.payload);
       return { ...state };
     default:
       return state;
