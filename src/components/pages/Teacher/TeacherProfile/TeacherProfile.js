7bdaf843a9a29dea7dd5c947dd062511e186eea8
Josh Whitwell <josh.whitwell@gmail.com>
2021-02-02 14:16:59 -0500

    udpates TeacherProfile with stakeholder fields

--- src/components/pages/Teacher/TeacherProfile/TeacherProfile.js
+++ src/components/pages/Teacher/TeacherProfile/TeacherProfile.js
@@ -15,10 +15,11 @@ import { ComponentTitle } from '../../../common';
 
 const TeacherProfile = props => {
   const { userId } = useSelector(state => state.authReducer);
-  const { profile } = props;
+  const { profile, fetchTeacherProfile } = props;
+
   useEffect(() => {
-    props.fetchTeacherProfile(userId - 10); // change this later with login
-  }, [props, userId]);
+    fetchTeacherProfile(userId - 10); // change this later with login
+  }, [fetchTeacherProfile, userId]);
 
   return (
     <>
@@ -27,26 +28,41 @@ const TeacherProfile = props => {
         <Label>Name:</Label>
         <p>{`${profile.first_name} ${profile.last_name}`}</p>
 
-        <Label>Account Status:</Label>
-        <p>{profile.account_status}</p>
-
-        <Label>Gender:</Label>
-        <p>{profile.gender}</p>
-
-        <Label>Address:</Label>
-        <p>{profile.address}</p>
-
-        <Label>Education Contact:</Label>
-        <p>{profile.name}</p>
-        <p>{profile.phone}</p>
+        <Label>Email:</Label>
         <p>{profile.email}</p>
-        <p>{profile.jobTitle}</p>
 
-        <Label>Notes:</Label>
-        <p>{profile.notes}</p>
+        <Label>Phone:</Label>
+        <p>{profile.phone}</p>
+
+        <Label>Primary Language:</Label>
+        <p>{profile.first_language}</p>
+
+        <Label>Other Languages:</Label>
+        <p>
+          {profile.other_language?.map((language, i) => {
+            if (i === profile.other_language.length - 1) {
+              return <span>{language}</span>;
+            } else {
+              return <span>{language}, </span>;
+            }
+          })}
+        </p>
+
+        <Label>City:</Label>
+        <p>{profile.city}</p>
+
+        <Label>Country</Label>
+        <p>{profile.country}</p>
 
         <Label>Time Zone:</Label>
         <p>{profile.time_zone}</p>
+
+        <Label>Classrooms:</Label>
+        <p>{profile.classrooms}</p>
+
+        <Label>Account Status:</Label>
+        <p>{profile.account_status}</p>
+
         <div className="teacherButtons">
           <Link to={`/teacher/${profile.id}/edit`}>
             <ThemeProvider theme={{ color: '#6ac66b' }}>
